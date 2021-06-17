446a9eadd113aa52270116285788b0486a94cea6
evum <evum12@gmail.com>
2021-06-17 18:13:13 +0300

    Some corrections in code

--- dl4j-examples/src/main/java/ru/mai/Bikes.java
+++ dl4j-examples/src/main/java/ru/mai/Bikes.java
@@ -3,8 +3,6 @@ package ru.mai;
 import org.datavec.api.records.reader.RecordReader;
 import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
 import org.datavec.api.split.FileSplit;
-import org.datavec.api.transform.TransformProcess;
-import org.datavec.api.transform.schema.Schema;
 import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
@@ -13,8 +11,6 @@ import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
-import org.deeplearning4j.examples.utils.DownloaderUtility;
-import org.nd4j.common.io.ClassPathResource;
 import org.nd4j.evaluation.classification.Evaluation;
 import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.api.ndarray.INDArray;
@@ -25,65 +21,47 @@ import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
 import org.nd4j.linalg.dataset.api.preprocessor.NormalizerStandardize;
 import org.nd4j.linalg.learning.config.Sgd;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
-public class Flowers {
+public class Bikes {
     static final int FEATURES_COUNT = 3;
     static final int CLASSES_COUNT = 2;
     static final int BATCHSIZE = 140;
+    static final int SEED = 6;
 
 
-    public static void main(String[] args) throws  Exception {
+    public static void main(String[] args) {
         FileGeneration file = new FileGeneration();
         try {
             file.gen();
             System.out.println();
             System.out.println("Файл создан");
         } catch (IOException e) {
-            System.out.println("Error!");
+            System.out.println("Error! Невозможно содать файл исходный данных!");
+            System.exit(0);
         }
 
 
-
-        int seed = 6;
-        DataSet allData;
-        try (RecordReader recordReader = new CSVRecordReader(0, ',')) {
-            recordReader.initialize(new FileSplit(new File("output.txt")));
-
-            DataSetIterator iterator = new RecordReaderDataSetIterator(recordReader, BATCHSIZE, FEATURES_COUNT, CLASSES_COUNT);
-            allData = iterator.next();
+        DataSet allData = new DataSet();
+        try {
+            allData = input(allData);
+        } catch (IOException | InterruptedException e) {
+            System.out.println(Arrays.toString(e.getStackTrace()));
+            System.exit(0);
         }
 
-        allData.shuffle(42);
-
-        DataNormalization normalizer = new NormalizerStandardize();
-        normalizer.fit(allData);
-        normalizer.transform(allData);
+        normalizing(allData);
 
         SplitTestAndTrain testAndTrain = allData.splitTestAndTrain(0.65);
         DataSet trainingData = testAndTrain.getTrain();
         DataSet testData = testAndTrain.getTest();
 
 
+        var configuration = neuralNetWorkBuilder();
 
-        MultiLayerConfiguration configuration = new NeuralNetConfiguration.Builder()
-                .seed(seed)
-                .activation(Activation.TANH)
-                .weightInit(WeightInit.XAVIER)
-                .updater(new Sgd(0.1))
-                .l2(1e-4)
-                .list()
-                .layer(0, new DenseLayer.Builder().nIn(FEATURES_COUNT).nOut(3).build())
-                .layer(1, new DenseLayer.Builder().nIn(3).nOut(3).build())
-                .layer(2, new OutputLayer.Builder(
-                    LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
-                    .activation(Activation.SOFTMAX)
-                    .nIn(3).nOut(CLASSES_COUNT).build())
-                .build();
         MultiLayerNetwork model = new MultiLayerNetwork(configuration);
         model.init();
         model.setListeners(new ScoreIterationListener(100));
@@ -93,6 +71,44 @@ public class Flowers {
             model.fit(trainingData);
         }
 
+        results(model, testData);
+    }
+
+    private static DataSet input(DataSet allData) throws IOException, InterruptedException {
+        RecordReader recordReader = new CSVRecordReader(0, ',');
+        recordReader.initialize(new FileSplit(new File("output.txt")));
+
+        DataSetIterator iterator = new RecordReaderDataSetIterator(recordReader, BATCHSIZE, FEATURES_COUNT, CLASSES_COUNT);
+        allData = iterator.next();
+        allData.shuffle(42);
+        return allData;
+    }
+
+    private static void normalizing(DataSet allData) {
+        DataNormalization normalizer = new NormalizerStandardize();
+        normalizer.fit(allData);
+        normalizer.transform(allData);
+    }
+
+    private static MultiLayerConfiguration neuralNetWorkBuilder() {
+        MultiLayerConfiguration configuration = new NeuralNetConfiguration.Builder()
+                .seed(SEED)
+                .activation(Activation.TANH)
+                .weightInit(WeightInit.XAVIER)
+                .updater(new Sgd(0.1))
+                .l2(1e-4)
+                .list()
+                .layer(0, new DenseLayer.Builder().nIn(FEATURES_COUNT).nOut(3).build())
+                .layer(1, new DenseLayer.Builder().nIn(3).nOut(3).build())
+                .layer(2, new OutputLayer.Builder(
+                        LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                        .activation(Activation.SOFTMAX)
+                        .nIn(3).nOut(CLASSES_COUNT).build())
+                .build();
+        return configuration;
+    }
+
+    private static void results(MultiLayerNetwork model, DataSet testData) {
         Evaluation eval = new Evaluation(CLASSES_COUNT);
         INDArray output = model.output(testData.getFeatures());
         eval.eval(testData.getLabels(), output);
